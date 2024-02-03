8248a454e2b7e37af5c94d9bdbbf8c75c63e61bb
Krunaljani9 <krunal.logicraysacademy011223@gmail.com>
2024-02-03 12:47:09 +0530

    web

---
+++ HTML_CSS/HTML/1st.py
@@ -0,0 +1,73 @@
+# print("hello")
+# a=10
+
+# if a > 5:
+#     print(a ," is greater \n than 5 ")
+#     print("This is another text")
+# else :
+#     print("a is less than 5")
+
+# b = "Hello, World!"
+# print(b[2:], len(b))
+# for x in b:
+#     print("aa")
+#     if 'W' in b:
+#         print("b")
+# name="Krunal"
+# def greet(name):
+#     print(f"you name is "+name)
+
+# greet(name)
+# list=[10,20,30,5]
+# tuple=(20,-1,'python',25.2)
+# print(tuple)
+# newlst=[]
+# a=0
+# def evenlst():
+#     global a,newlst
+#     a=1
+#     newlst=[var for var in list if var%2==0]
+#     print("Hello")
+# evenlst()
+# print("new list",newlst)
+# print(a)
+# def sayHi(name):
+#     return f'Hi {name}'
+# print(sayHi('Krunal'))
+
+# def get_net_price(price, discount):
+#     return price * (1-discount/100)
+# net_price = get_net_price(100, 10)
+# print(net_price)
+
+# def get_info(name,age,city):
+#     return f'{name} {age} {city}'
+# print(get_info(city='ahmedabad',age='35',name='Krunal'))
+# def recursive(start):
+#     print(start)
+#     if(start==0):
+#         return 0    
+
+#     return recursive(start-1)
+
+# for var in range(3,1,-1):
+# print(recursive(var))
+    # print(var)
+# recursive(3)
+# print("Hello")
+
+# sum = 0
+# for num in range(101):
+#     sum += num
+
+# print(sum)
+# for i in range(1,5):
+#     # print(i)
+#     for j in range(i):
+#         print(j+1,end=' ')
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(j+1,end=" ")
+#     print()
+    
